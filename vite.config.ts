0d6fa1f5fc8c181f10cee3729785e5809569e574
UncleK <starkapin@gmail.com>
2025-04-06 00:09:42 +0900

    remove manual chunking configuration and retain only essential plugins in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,36 +4,6 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          emotion: ['@emotion/react', '@emotion/styled'],
-          mui: [
-            '@mui/icons-material',
-            '@mui/material',
-            '@mui/system',
-            '@mui/utils',
-            '@mui/x-charts',
-            '@mui/x-data-grid',
-            '@mui/x-data-grid-pro',
-            '@mui/x-date-pickers',
-            '@mui/x-date-pickers-pro',
-            '@mui/x-tree-view',
-          ],
-          vendor: [
-            '@react-spring/web',
-            'clsx',
-            'dayjs',
-            'global',
-            'react',
-            'react-dom',
-            'react-router-dom',
-          ],
-        },
-      },
-    },
-  },
+  plugins: [react()],
   base: '/app/',
 });
